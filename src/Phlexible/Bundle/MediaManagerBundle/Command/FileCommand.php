8490b9821ca580b868e49d222c2eb6ab1059fbe5
Stephan Wentz <swentz@brainbits.net>
2015-05-26 14:43:13 +0200

    add file command

---
+++ src/Phlexible/Bundle/MediaManagerBundle/Command/FileCommand.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * phlexible
+ *
+ * @copyright 2007-2013 brainbits GmbH (http://www.brainbits.net)
+ * @license   proprietary
+ */
+
+namespace Phlexible\Bundle\MediaManagerBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * File command
+ *
+ * @author Stephan Wentz <sw@brainbits.net>
+ */
+class FileCommand extends ContainerAwareCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('media-manager:file')
+            ->setDefinition(
+                [
+                    new InputArgument('id', InputArgument::REQUIRED, 'File ID.'),
+                    new InputOption('with-attributes', null, InputOption::VALUE_NONE, 'Include attributes.'),
+                ]
+            )
+            ->setDescription('Show file info');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $fileId = $input->getArgument('id');
+
+        $volumeManager = $this->getContainer()->get('phlexible_media_manager.volume_manager');
+        $file = $volumeManager->getByFileId($fileId)->findFile($fileId);
+
+        $data = array(
+            'ID'             => $file->getId(),
+            'Folder ID'      => $file->getFolderId(),
+            'Path'           => $file->getPhysicalPath(),
+            'Created At'     => $file->getCreatedAt()->format('Y-m-d H:i:s'),
+            'Create User ID' => $file->getCreateUserId(),
+            'Modified At'    => $file->getModifiedAt()->format('Y-m-d H:i:s'),
+            'Modify User ID' => $file->getModifyUserId(),
+            'Hash'           => $file->getHash(),
+            'MimeType'       => $file->getMimeType(),
+            'Name'           => $file->getName(),
+            'Size'           => $file->getSize(),
+            'Version'        => $file->getVersion(),
+        );
+
+        $table = new Table($output);
+        foreach ($data as $key => $value) {
+            $table->addRow(array($key, $value));
+        }
+        $table->render();
+
+        if ($input->getOption('with-attributes')) {
+            $output->writeln('');
+            $output->writeln('Attributes:');
+            $table = new Table($output);
+            $table->setHeaders(array('Key', 'Value'));
+            foreach ($file->getAttributes() as $key => $value) {
+                $table->addRow(array($key, $value));
+            }
+            $table->render();
+        }
+
+        return 0;
+    }
+}
